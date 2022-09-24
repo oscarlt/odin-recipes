45823ed21a92840cfbecd10b81e4eebfeb3eb75d
Oscar T <oscarlt@gmail.com>
2022-09-24 00:11:08 -0700

    .

---
+++ recipes/script.js
@@ -0,0 +1,14 @@
+const body = document.body
+
+// const div = document.createElement("div")
+
+// div.innerText = "Hello World"
+
+// div.textContent = "Hello World"
+// body.append(div)
+
+const div = document.querySelector("div")
+
+console.log(div.textContent)
+console.log(div.innerText)
+
